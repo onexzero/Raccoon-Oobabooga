708a721f0fa5c9a5f38ca0356487d8ed9dbf0d9d
Mr. raccoon <onexzero@naver.com>
2024-06-14 15:12:59 +0900

    Additional parameter added

--- raccoon_oobabooga.py
+++ raccoon_oobabooga.py
@@ -16,15 +16,19 @@ class RaccoonOobabooga:
         "character": ( "STRING", {
           "default": "Describer",
         }),        
+        "seed":        ("INT",   {"default": 0,   "min": 0, "max": 0xffffffffffffffff}),
+        "max_tokens":  ("INT",   {"default": 200, "min": 0, "max": 0xffffffffffffffff}),
+        "temperature": ("FLOAT", {"default": 1.0, "min": 0, "max": 0xffffffffffffffff}),
+        "top_p":       ("FLOAT", {"default": 0.9, "min": 0, "max": 0xffffffffffffffff}),        
       }
     }
 
   RETURN_TYPES = ("STRING",)
-  RETURN_NAMES = ("response",)
+  RETURN_NAMES = ("STRING",)
   FUNCTION = "oobabooga"
   CATEGORY = "Raccoon/Oobabooga"
 
-  def oobabooga(self, host, prompt, character):
+  def oobabooga(self, host, prompt, character,seed, max_tokens, temperature, top_p):
     url = "/v1/chat/completions"
     url = host + url
 
@@ -41,10 +45,14 @@ class RaccoonOobabooga:
             "content": prompt,
           }
       ],      
+      "max_tokens":  max_tokens,
+      "temperature": temperature,
+      "top_p":       top_p,
+      "seed":        seed,
     }
 
     response = requests.post(url, headers=headers, json=data, verify=False)
     response.raise_for_status()  # Raise exception for non-2xx responses
     answer = response.json()["choices"][0]["message"]["content"]    
-
-    return(answer, )
+    print("LLM RESP:",answer)
+    return (answer,)
